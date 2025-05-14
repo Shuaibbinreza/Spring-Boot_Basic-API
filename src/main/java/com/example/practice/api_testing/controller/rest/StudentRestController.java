bde6538f3ca82c4f24798f5b007ce7cfb26d0dc9
Shuaib <shuaib6950@gmail.com>
2025-05-14 19:19:10 +0600

    Common request mapping in StudentRestController

--- src/main/java/com/example/practice/api_testing/controller/rest/StudentRestController.java
+++ src/main/java/com/example/practice/api_testing/controller/rest/StudentRestController.java
@@ -1,4 +1,4 @@
-package com.example.practice.api_testing.controller;
+package com.example.practice.api_testing.controller.rest;
 
 import com.example.practice.api_testing.model.domain.Student;
 import com.example.practice.api_testing.model.dto.CreateStudent;
@@ -7,23 +7,25 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
 
+//Documentation with Swagger
 @RestController
+@RequestMapping("/api")
 public class StudentRestController {
     List<Student> students = new ArrayList<>();
 
-    @GetMapping("/api/students")
+    @GetMapping("students")
     public List<Student> getStudents() {
         return students;
     }
 
-    @PostMapping("/api/students")
-    public Student addStudent(String name, Integer age, String gender) {
-        Student student = new Student(name, age, gender);
-        students.add(student);
-        return student;
-    }
+//    @PostMapping("/students")
+//    public Student addStudent(String name, Integer age, String gender) {
+//        Student student = new Student(name, age, gender);
+//        students.add(student);
+//        return student;
+//    }
 
-    @PostMapping("/api/students-add")
+    @PostMapping("students-add")
     public Student StudentAdd(@RequestBody CreateStudent cs) {
         String name = cs.getName();
         Integer age = cs.getAge();
--- src/main/java/com/example/practice/api_testing/controller/web/RootController.java
+++ src/main/java/com/example/practice/api_testing/controller/web/RootController.java
@@ -1,4 +1,4 @@
-package com.example.practice.api_testing.controller;
+package com.example.practice.api_testing.controller.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
