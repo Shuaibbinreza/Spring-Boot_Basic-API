403a1a4f28c20fd96f5a53b3be1d0a1e4aa2b9a9
Shuaib <shuaib6950@gmail.com>
2025-06-14 18:10:34 +0600

    intro to lombok

--- src/main/java/com/example/practice/api_testing/model/domain/Student.java
+++ src/main/java/com/example/practice/api_testing/model/domain/Student.java
@@ -1,31 +1,21 @@
 package com.example.practice.api_testing.model.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
 public class Student {
-    private final Long id;
-    private final String name;
-    private final Integer age;
-    private final String gender;
+    @Getter
+    private Long id;
 
-    public Student(Long id, String name, Integer age, String gender) {
-        this.id = id;
-        this.name = name;
-        this.age = age;
-        this.gender = gender;
-    }
+    @Getter
+    private String name;
 
-    public String getName() {
-        return name;
-    }
+    @Getter
+    private Integer age;
 
-    public Integer getAge() {
-        return age;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public Long getId() {
-        return id;
-    }
+    @Getter
+    private String gender;
 }
--- src/main/java/com/example/practice/api_testing/persistence/entity/StudentEntity.java
+++ src/main/java/com/example/practice/api_testing/persistence/entity/StudentEntity.java
@@ -4,47 +4,24 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
 @Table(name = "student")
 @Entity
+@Getter
 public class StudentEntity {
 
     @Id
     @GeneratedValue
     private Long id;
+
+    @Setter
     private String name;
+
+    @Setter
     private Integer age;
+
+    @Setter
     private String gender;
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public Long getId() {
-        return id;
-    }
 }
