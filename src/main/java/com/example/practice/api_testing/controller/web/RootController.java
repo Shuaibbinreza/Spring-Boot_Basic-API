b9f3dccb00bbc57697c4478cdf6ec7333253c2cf
Shuaib <shuaib6950@gmail.com>
2025-06-14 22:40:30 +0600

    Log with slf4j

--- src/main/java/com/example/practice/api_testing/controller/web/RootController.java
+++ src/main/java/com/example/practice/api_testing/controller/web/RootController.java
@@ -1,5 +1,6 @@
 package com.example.practice.api_testing.controller.web;
 
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import com.example.practice.api_testing.config.ResumeConfig;
 import com.example.practice.api_testing.service.StudentService;
 
+@Slf4j
 @Controller
 public class RootController {
     @Autowired
@@ -22,14 +24,14 @@ public class RootController {
     @Value("${site.description}")
     String siteDescription;
 
-    private static Logger logger = LoggerFactory.getLogger(RootController.class);
+//    private static Logger logger = LoggerFactory.getLogger(RootController.class);
 
     @Autowired
     StudentService studentService;
     
     @GetMapping
     public String indexPage(Model model) {
-        logger.debug("Setting the model data");
+        log.debug("Setting the model data");
         model.addAttribute("siteTitle", siteTitle);
         model.addAttribute("siteDescription", siteDescription);
         model.addAttribute("personalInfo", resumeConfig.getPersonalInfo());
@@ -37,7 +39,7 @@ public class RootController {
         model.addAttribute("experience", resumeConfig.getExperience());
         model.addAttribute("skills", resumeConfig.getSkills());
         model.addAttribute("students", studentService.getStudents());
-        logger.debug("Model data setup done");
+        log.debug("Model data setup done");
         return "index";
     }
 }
--- src/main/java/com/example/practice/api_testing/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/practice/api_testing/exception/GlobalExceptionHandler.java
@@ -3,21 +3,20 @@ package com.example.practice.api_testing.exception;
 import java.net.URI;
 
 import com.example.practice.api_testing.exception.custom.NotFoundException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-    private static Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
     // Checking the global exception class
     @ExceptionHandler(NotFoundException.class)
     public ProblemDetail handleNotFoundException(NotFoundException e){
-        logger.error("Resource not found: {}", e.getMessage());
+        log.error("Resource not found: {}", e.getMessage());
         ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, e.getMessage());
         problemDetail.setTitle("Resource Not FOund");
         problemDetail.setType(URI.create("https://example.com/not-found"));
@@ -27,13 +26,13 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentTypeMismatchException.class)
     public ProblemDetail handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException e) {
-        logger.error("Invalid Input Type: {}", e.getMessage());
+        log.error("Invalid Input Type: {}", e.getMessage());
         return ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, "Invalid Input Type");
     }
 
     @ExceptionHandler(Exception.class)
     public ProblemDetail handleGenericException(Exception e){
-        logger.error("Generic Exception: ", e);
+        log.error("Generic Exception: ", e);
         return ProblemDetail.forStatusAndDetail(HttpStatus.INTERNAL_SERVER_ERROR, "Something Went wrong in server side");
     }
 }
